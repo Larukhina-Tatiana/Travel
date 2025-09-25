d1b36f76e385ee60b92b642933b852e48c159bd2
Татьяна <ltasya72@gmail.com>
2025-09-25 21:02:43 +0300

    пуш в docs

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -2,7 +2,7 @@
 import { watch } from "fs";
 import * as nodePath from "path";
 const rootFolder = nodePath.basename(nodePath.resolve());
-const buildFolder = `./build`;
+const buildFolder = `./docs`;
 const srcFolder = `./src`;
 
 export const path = {
